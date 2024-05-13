a4320285332619b23cc278502d6820ec41ee0197
Jorden Dickerson <jordendickerson24@gmail.com>
2024-05-13 17:15:19 -0600

    Html file

--- script.js
+++
@@ -1,7 +0,0 @@
-const form = document.getElementById("info-form");
-
-function handleSubmit(event) {
-  event.preventDefault();
-  console.log(":)");
-}
-form.addEventListener("submit", () => handleSubmit);
