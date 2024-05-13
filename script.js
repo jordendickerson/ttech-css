5f31c67af32a164e01cb866c28fec7504b8a1071
Jorden <jordendickerson24@gmail.com>
2024-05-12 22:00:08 -0600

    AAAAAAH

---
+++ script.js
@@ -0,0 +1,7 @@
+const form = document.getElementById("info-form");
+
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log(":)");
+}
+form.addEventListener("submit", () => handleSubmit);
